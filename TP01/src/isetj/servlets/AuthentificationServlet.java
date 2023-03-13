b212b2b16c36597191acac3b62f9d3fe31641d2e
nessrinemezni <127732456+nessrinemezni@users.noreply.github.com>
2023-03-13 10:07:19 +0100

    Add files via upload

---
+++ TP01/src/isetj/servlets/AuthentificationServlet.java
@@ -0,0 +1,59 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthentificationServlet
+ */
+@WebServlet("/AuthentificationServlet")
+public class AuthentificationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthentificationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String login=request.getParameter("login");
+		String password=request.getParameter("password");
+		if("dsi".equals(login)&& "dsi".equals(password)) {
+		PrintWriter out=response.getWriter();
+		out.println("<html><body>");
+		out.println("<h2>authentification réussite</h2>");
+		out.println("</body></html>");}
+		else {
+			PrintWriter out=response.getWriter();
+			out.println("<html><body>");
+			out.println("<h2>aEchec à l'authentification</h2>");
+			out.println("</body></html>");}
+		}
+		
+				
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ TP01/src/isetj/servlets/CompteurServlet.java
@@ -0,0 +1,47 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Compteur")
+public class CompteurServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CompteurServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    private int compteur = 0;
+    
+    @SuppressWarnings("deprecation")
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+      HttpSession session = request.getSession(true);
+      Integer compteurSession = (Integer)session.getAttribute("compteur");
+      if (compteurSession == null) {
+        compteurSession = new Integer(1);
+      } else {
+        compteurSession = new Integer(compteurSession.intValue() + 1);
+      }
+      session.setAttribute("compteur", compteurSession);
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      out.println("<html>");
+      out.println("<head><title>CompteurServlet</title></head>");
+      out.println("<body>");
+      out.println("<h1>CompteurServlet</h1>");
+      out.println("<p>Nombre d'appels pendant cette session: " + compteurSession.intValue() + "</p>");
+      out.println("</body>");
+      out.println("</html>");
+	}
+}
